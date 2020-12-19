ff9be0d283032b7707aa292ce33151e22377a28e
momchilmitev <momchilmitev87@gmail.com>
2020-12-19 09:00:24 +0100

    added custom hook for fetching videos

--- src/components/App.js
+++ src/components/App.js
@@ -3,36 +3,19 @@ import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetails from "./VideoDetails";
-import youtube from "../apis/youtube";
-
-const KEY = "AIzaSyArj6mVcDmeGxzlo0xNEqIgNGn8sUplWrU";
+import useVideos from "../hooks/useVideos";
 
 const App = () => {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("acura");
 
   useEffect(() => {
-    onFormSubmit("acura");
-  }, []);
-
-  const onFormSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-        part: "snippet",
-        type: "video",
-        maxResults: 5,
-        key: KEY,
-      },
-    });
-
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onFormSubmit} />
+      <SearchBar onFormSubmit={search} />
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
